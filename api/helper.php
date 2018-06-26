92a3590641fce3c0e9669983aa1fb4a8d7457e59
Luis Ricardo Sanchez <lsanchez@esferasoluciones.com>
2018-06-26 00:11:17 -0500

    orm, api de login y helper listos

---
+++ api/helper.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'lib/rb-mysql.php';
+header('Content-type: application/json');
+header("access-control-allow-origin: *");
+
+$username = 'espheras_dbuser';
+$password = 'Goingupinlife123';
+$database = 'espheras_busport';
+$dbhost = '162.210.98.37';
+
+R::setup( 'mysql:host=' . $dbhost .';dbname=' . $database, $username, $password );
+R::ext('xdispense', function( $type ){
+    return R::getRedBean()->dispense( $type );
+});
+R::freeze( TRUE );
+
+function retrieveJsonPostData() {
+    // get the raw POST data
+    $rawData = file_get_contents("php://input");
+
+    // this returns null if not valid json
+    return json_decode($rawData);
+}
